b9a2fdab29bb049a78b29cc327a1b824347ba140
Nunu998 <84434671+Nunu998@users.noreply.github.com>
2021-06-09 17:41:42 +0700

    Project IDEA

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+ public static void main(String[] args){
+     String namaDepan = "Ibnu";
+     String namaTengah = "Azmi";
+     String namaBelakang = "Nazira";
+     int usia = 20;
+     int targetTahunKuliah = 4;
+     double ipk = 3.69;
+     char nilaiAbjad = 'A';
+     boolean tampan = true;
+
+     System.out.println(namaDepan + namaTengah + namaBelakang);
+ }
+}
